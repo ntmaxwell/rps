300755886048fdc44378318b0e9f762ccf71593e
Nate Maxwell <ntmaxwell@yahoo.com>
2024-10-01 13:35:23 -0700

    Create computer Choice function for RPS
    Allow the computer to make a choice between Rock,
    Paper, and Scissors. Using a RNG between 0-2 and
    a switch statement based on the result to assign
    R/P/S.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+function getComputerChoice() {
+
+    let compChoice = (Math.floor(Math.random() * 3));
+
+    switch(compChoice) {
+        case 0:
+            console.log("Rock");
+            break;
+        case 1:
+            console.log("Paper");
+            break;
+        case 2:
+            console.log("Scissors");
+            break;
+        default:
+            console.log("This should never happen");
+    }
+}
+
+let ComputerChoice = getComputerChoice();
+
+console.log(ComputerChoice);
