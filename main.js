cf620def4683d60f67fccf3150a7b1fca886f6dc
Nate Maxwell <ntmaxwell@yahoo.com>
2024-10-03 21:53:49 -0700

    Add function to play a single round.
    Add function playRound that takes compChoice &
    humanChoice as arguments then figures out the
    round winner.

--- main.js
+++ main.js
@@ -1,22 +1,74 @@
+// Global Variables for Player's Scores
+
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
 
     let compChoice = (Math.floor(Math.random() * 3));
 
     switch(compChoice) {
         case 0:
-            console.log("Rock");
-            break;
+            return "rock";
         case 1:
-            console.log("Paper");
-            break;
+            return "paper";
         case 2:
-            console.log("Scissors");
-            break;
+            return "scissors";
         default:
             console.log("This should never happen");
     }
 }
 
-let ComputerChoice = getComputerChoice();
+function getHumanChoice() {
 
-console.log(ComputerChoice);
+    let userChoice = prompt("Choose Rock, Paper, or Scissors:");
+
+    switch(userChoice.toLowerCase()) {
+        case "rock":
+            return "rock";
+        case "paper":
+            return "paper";
+        case "scissors":
+            return "scissors";
+        default:
+            console.log("Invalid choice. Please choose Rock, Paper, or Scissors.");
+    }
+}
+
+function playRound(getHumanChoice, getComputerChoice) {
+
+    if (pc === "rock" && npc === "paper") {
+        return "You lose! Paper Beats Rock!";
+    }
+    else if (pc === "rock" && npc === "scissors") {
+        return "You win! Rock Beats Scissors!";
+    }
+    else if (pc === "rock" && npc === "rock") {
+        return "It's a tie! Both players chose Rock!";
+    }
+    else if (pc === "paper" && npc === "rock") {
+        return "You win! Paper Beats Rock!";
+    }
+    else if (pc === "paper" && npc === "scissors") {
+        return "You lose! Scissors beats Paper!"
+    }
+    else if (pc === "paper" && npc === "paper") {
+        return "It's a tie! Both players chose Paper!"
+    }
+    else if (pc === "scissors" && npc === "rock") {
+        return "You lose! Rock beats Scissors!"
+    }
+    else if (pc === "scissors" && npc === "paper") {
+        return "You win! Scissors beats Paper!"
+    }
+    else if (pc === "scissors" && npc === "scissors") {
+        return "It's a tie! Both players chose Scissors!"
+    }
+}
+
+let pc = getHumanChoice();
+let npc = getComputerChoice();
+let round = playRound();
+
+console.log(round);
